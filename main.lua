4bf79b0d57ac9e2fcd7558d29c8fe5269701c3bf
Sávio Carlos Martins Costa <saviocmc@protonmail.com>
2021-09-13 16:41:06 -0300

    Aula 03-09: Mudando o posicionamento inicial do avião

--- main.lua
+++ main.lua
@@ -1,8 +1,21 @@
+window = {
+    title = 'Plain vs Meteors',
+    size = { x = 320, y = 480 },
+    background = {
+        src = 'images/background.png',
+    }
+}
+
 plane = {
     src = 'images/14bis.png',
     size = { x = 64, y = 63 },
     position = { x = 0, y = 0 },
 }
+
+function setupPlainPosition()
+    plane.position.x = window.size.x / 2 - plane.size.x / 2
+    plane.position.y = window.size.y - plane.size.y / 2
+end
 
 function updatePlainPosition()
     if love.keyboard.isDown('w') then
@@ -20,11 +33,11 @@ function updatePlainPosition()
 end
 
 function love.load()
-    love.window.setMode(320, 480, { resizable = false })
-    love.window.setTitle('14bis vs Meteoros')
-    background = love.graphics.newImage('images/background.png')
+    love.window.setTitle(window.title)
+    love.window.setMode(window.size.x, window.size.y, { resizable = false })
+    window.background.ref = love.graphics.newImage(window.background.src)
     plane.ref = love.graphics.newImage(plane.src)
-    x, y, w, h = 20, 20, 60, 20
+    setupPlainPosition()
 end
 
 function love.update(dt)
@@ -32,6 +45,6 @@ function love.update(dt)
 end
 
 function love.draw()
-    love.graphics.draw(background, 0, 0)
+    love.graphics.draw(window.background.ref, 0, 0)
     love.graphics.draw(plane.ref, plane.position.x, plane.position.y)
 end
