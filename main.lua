04dd511cbf9e189fd0cc63b3d4c0df4d58165db3
Sávio Carlos Martins Costa <saviocmc@protonmail.com>
2021-09-13 19:08:24 -0300

    Aula 05-04: Representando a colisão do avião

--- main.lua
+++ main.lua
@@ -8,7 +8,7 @@ window = {
 
 plane = {
     src = 'images/14bis.png',
-    size = { x = 64, y = 63 },
+    size = { x = 55, y = 63 },
     position = { x = 0, y = 0 },
 }
 
@@ -32,14 +32,22 @@ function updatePlainPosition()
     end
 end
 
+function explodePlane()
+    plane.src = 'images/explosao_nave.png'
+    plane.ref = love.graphics.newImage(plane.src)
+end
+
 meteor_src = 'images/meteoro.png'
 meteors_max_size = 12
 meteors = {}
 
 function addMeteor()
     newMeteor = {
-        x = math.random(window.size.x),
-        y = -50,
+        size = { x = 50, y = 40 },
+        position = {
+            x = math.random(window.size.x),
+            y = -50,
+        },
         x_shift = math.random(-1,1),
         mass = math.random(3),
     }
@@ -48,17 +56,34 @@ end
 
 function updateMeteorsPosition()
     for index,meteor in ipairs(meteors) do
-        meteor.y = meteor.y + meteor.mass
-        meteor.x = meteor.x + meteor.x_shift
+        meteor.position.y = meteor.position.y + meteor.mass
+        meteor.position.x = meteor.position.x + meteor.x_shift
     end
 end
 
 function removeInvisibleMeteors()
     for index = #meteors,1,-1 do
-        if meteors[index].y > window.size.y then
+        if meteors[index].position.y > window.size.y then
             table.remove(meteors, index)
         end
     end
+end
+
+function hasOverlay(o1, o2)
+    return
+        o1.position.x < o2.position.x + o2.size.x and
+        o2.position.x < o1.position.x + o1.size.x and
+        o1.position.y < o2.position.y + o2.size.y and
+        o2.position.y < o1.position.y + o1.size.y 
+end
+
+function isPlaneCrashed()
+    for index,meteor in ipairs(meteors) do
+        if hasOverlay(plane, meteor) then
+            return true
+        end
+    end
+    return false
 end
 
 function love.load()
@@ -72,18 +97,23 @@ function love.load()
 end
 
 function love.update(dt)
+    if(GAME_OVER) then return end
     updatePlainPosition()
     updateMeteorsPosition()
     removeInvisibleMeteors()
     if #meteors < meteors_max_size then
         addMeteor()
     end
+    if isPlaneCrashed() then
+        explodePlane()
+        GAME_OVER = true
+    end
 end
 
 function love.draw()
     love.graphics.draw(window.background.ref, 0, 0)
     for index,meteor in ipairs(meteors) do
-        love.graphics.draw(meteor_ref, meteor.x, meteor.y)
+        love.graphics.draw(meteor_ref, meteor.position.x, meteor.position.y)
     end
     love.graphics.draw(plane.ref, plane.position.x, plane.position.y)
 end
