e7e5278df4586282b99cf161d68b13959cfb7c07
Sávio Carlos Martins Costa <saviocmc@protonmail.com>
2021-09-13 15:05:16 -0300

    Aula 03-05: Manipulando objetos pelo teclado

--- main.lua
+++ main.lua
@@ -1,17 +1,26 @@
--- Load some default values for our rectangle.
 function love.load()
+    love.window.setMode(320, 480, { resizable = false })
+    love.window.setTitle('14bis vs Meteoros')
+    background = love.graphics.newImage('images/background.png')
     x, y, w, h = 20, 20, 60, 20
 end
 
--- Increase the size of the rectangle every frame.
 function love.update(dt)
-    w = w + 1
-    h = h + 1
+    if love.keyboard.isDown('w') then
+        y = y - 1
+    end
+    if love.keyboard.isDown('s') then
+        y = y + 1
+    end
+    if love.keyboard.isDown('a') then
+        x = x - 1
+    end
+    if love.keyboard.isDown('d') then
+        x = x + 1
+    end
 end
 
--- Draw a coloured rectangle.
 function love.draw()
-    -- In versions prior to 11.0, color component values are (0, 102, 102)
-    love.graphics.setColor(0, 0.4, 0.4)
+    love.graphics.draw(background, 0, 0)
     love.graphics.rectangle("fill", x, y, w, h)
 end
