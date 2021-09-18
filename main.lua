6fe72bc19bc19c0d733cfaa929e6920f3bb418ef
Sávio Carlos Martins Costa <saviocmc@protonmail.com>
2021-09-17 22:35:00 -0300

    Small refactor to organize the code

--- main.lua
+++ main.lua
@@ -1,23 +1,40 @@
-window = {
+local images = {}
+
+local function loadImages()
+    images.background = love.graphics.newImage('images/background.png')
+    images.plane = love.graphics.newImage('images/plane.png')
+    images.meteor = love.graphics.newImage('images/meteor.png')
+    images.shot = love.graphics.newImage('images/shot.png')
+    images.gameover = love.graphics.newImage('images/gameover.png')
+    images.winner = love.graphics.newImage('images/winner.png')
+end
+
+local audios = {}
+
+local function loadAudioTracks()
+        audios.background = love.audio.newSource('audios/background.wav', 'static')
+        audios.shot = love.audio.newSource('audios/shot.wav', 'static')
+        audios.explosion = love.audio.newSource('audios/explosion.wav', 'static')
+        audios.gameover = love.audio.newSource('audios/gameover.wav', 'static')
+        audios.winner = love.audio.newSource('audios/winner.wav', 'static')
+end
+
+local window = {
     title = 'Plain vs Meteors',
     size = { x = 320, y = 480 },
-    background = {
-        src = 'images/background.png',
-    }
 }
 
-plane = {
-    src = 'images/plane.png',
+local plane = {
     size = { x = 56, y = 62 },
     position = { x = 0, y = 0 },
 }
 
-function setupPlainPosition()
+local function setupPlainPosition()
     plane.position.x = window.size.x / 2 - plane.size.x / 2
     plane.position.y = window.size.y - plane.size.y
 end
 
-function updatePlainPosition()
+local function updatePlainPosition()
     if love.keyboard.isDown('w') then
         plane.position.y = plane.position.y - 1
     end
@@ -32,7 +49,7 @@ function updatePlainPosition()
     end
 end
 
-function explodePlane()
+local function explodePlane()
     plane.src = 'images/explosion.png'
     plane.ref = love.graphics.newImage(plane.src)
     audios.background:stop()
@@ -40,12 +57,11 @@ function explodePlane()
     audios.gameover:play()
 end
 
-meteor_src = 'images/meteor.png'
-meteors_max_size = 12
-meteors = {}
+local meteors_max_size = 12
+local meteors = {}
 
-function addMeteor()
-    newMeteor = {
+local function addMeteor()
+    local newMeteor = {
         size = { x = 56, y = 42 },
         position = {
             x = math.random(window.size.x),
@@ -57,7 +73,7 @@ function addMeteor()
     table.insert(meteors, newMeteor)
 end
 
-function updateMeteorsPosition()
+local function updateMeteorsPosition()
     for index = #meteors,1,-1 do
         local meteor = meteors[index]
         if meteor.position.y < window.size.y then
@@ -69,11 +85,10 @@ function updateMeteorsPosition()
     end
 end
 
-shot_src = 'images/shot.png'
-shots = {}
+local shots = {}
 
-function shoot()
-    shot = {
+local function shoot()
+    local shot = {
         size = { x = 12, y = 12 },
         position = {
             x = plane.position.x + plane.size.x / 2 - 6,
@@ -84,7 +99,7 @@ function shoot()
     audios.shot:play()
 end
 
-function updateShotsPosition()
+local function updateShotsPosition()
     for index = #shots,1,-1 do
         if shots[index].position.y > 0 then
             shots[index].position.y = shots[index].position.y - 1
@@ -94,14 +109,22 @@ function updateShotsPosition()
     end
 end
 
-pointsCount = 0
-pointsToWin = 10
+local pointsCount = 0
+local pointsToWin = 10
 
-function removeShootedMeteors()
+local function hasOverlay(o1, o2)
+    return
+        o1.position.x < o2.position.x + o2.size.x and
+        o2.position.x < o1.position.x + o1.size.x and
+        o1.position.y < o2.position.y + o2.size.y and
+        o2.position.y < o1.position.y + o1.size.y
+end
+
+local function removeShootedMeteors()
     for i = #meteors,1,-1 do
-        meteor = meteors[i]
+        local meteor = meteors[i]
         for j = #shots,1,-1 do
-            shot = shots[j]
+            local shot = shots[j]
             if hasOverlay(meteor, shot) then
                 table.remove(meteors, i)
                 table.remove(shots, j)
@@ -112,16 +135,8 @@ function removeShootedMeteors()
     end
 end
 
-function hasOverlay(o1, o2)
-    return
-        o1.position.x < o2.position.x + o2.size.x and
-        o2.position.x < o1.position.x + o1.size.x and
-        o1.position.y < o2.position.y + o2.size.y and
-        o2.position.y < o1.position.y + o1.size.y
-end
-
-function isPlaneCrashed()
-    for index,meteor in ipairs(meteors) do
+local function isPlaneCrashed()
+    for _,meteor in ipairs(meteors) do
         if hasOverlay(plane, meteor) then
             return true
         end
@@ -137,37 +152,18 @@ function love.keypressed(key)
     end
 end
 
-function loadImages()
-    window.background.ref = love.graphics.newImage(window.background.src)
-    plane.ref = love.graphics.newImage(plane.src)
-    meteor_ref = love.graphics.newImage(meteor_src)
-    shot_ref = love.graphics.newImage(shot_src)
-    gameover_ref = love.graphics.newImage('images/gameover.png')
-    winner_ref = love.graphics.newImage('images/winner.png')
-end
-
-function loadAudioTracks()
-    audios = {
-        background = love.audio.newSource('audios/background.wav', 'static'),
-        shot = love.audio.newSource('audios/shot.wav', 'static'),
-        explosion = love.audio.newSource('audios/explosion.wav', 'static'),
-        gameover = love.audio.newSource('audios/gameover.wav', 'static'),
-        winner = love.audio.newSource('audios/winner.wav', 'static'),
-    }
-end
-
 function love.load()
     math.randomseed(os.time())
     love.window.setTitle(window.title)
     love.window.setMode(window.size.x, window.size.y, { resizable = false })
-    loadImages() 
+    loadImages()
     loadAudioTracks()
     audios.background:setLooping(true)
     audios.background:play()
     setupPlainPosition()
 end
 
-function love.update(dt)
+function love.update()
     if(GAME_OVER or WINNER) then return end
     updatePlainPosition()
     updateMeteorsPosition()
@@ -189,19 +185,27 @@ function love.update(dt)
 end
 
 function love.draw()
-    love.graphics.draw(window.background.ref, 0, 0)
+    love.graphics.draw(images.background, 0, 0)
     for _,meteor in ipairs(meteors) do
-        love.graphics.draw(meteor_ref, meteor.position.x, meteor.position.y)
+        love.graphics.draw(images.meteor, meteor.position.x, meteor.position.y)
     end
-    love.graphics.draw(plane.ref, plane.position.x, plane.position.y)
+    love.graphics.draw(images.plane, plane.position.x, plane.position.y)
     for _,shot in ipairs(shots) do
-        love.graphics.draw(shot_ref, shot.position.x, shot.position.y)
+        love.graphics.draw(images.shot, shot.position.x, shot.position.y)
     end
     love.graphics.print('Meteors to destroy: '..pointsToWin-pointsCount)
     if GAME_OVER then
-        love.graphics.draw(gameover_ref, window.size.x/2 - gameover_ref:getWidth()/2, window.size.y/2 - gameover_ref:getHeight()/2)
+        love.graphics.draw(
+            images.gameover,
+            window.size.x/2 - images.gameover:getWidth()/2,
+            window.size.y/2 - images.gameover:getHeight()/2
+        )
     end
     if WINNER then
-        love.graphics.draw(winner_ref, window.size.x/2 - winner_ref:getWidth()/2, window.size.y/2 - winner_ref:getHeight()/2)
+        love.graphics.draw(
+            images.winner,
+            window.size.x/2 - images.winner:getWidth()/2,
+            window.size.y/2 - images.winner:getHeight()/2
+        )
     end
 end
